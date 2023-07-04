ffc5025c7c2f91fc9de35a5266455e7dbe43bed8
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2023-07-04 11:55:41 +0300

    fix: update embeddings script to work with our md files (#1)
    * removed almost all mdx files
    
    * fix: adjusted md parsing and embeddings
    
    * fix: do not remove front matter
    
    * fix: do not clean md files
    
    * fix: added all docs pages from the nx repo
    
    * add throttling to requests

--- lib/generate-embeddings.ts
+++ lib/generate-embeddings.ts
@@ -1,89 +1,24 @@
 import { createClient } from '@supabase/supabase-js'
 import { createHash } from 'crypto'
 import dotenv from 'dotenv'
-import { ObjectExpression } from 'estree'
 import { readdir, readFile, stat } from 'fs/promises'
 import GithubSlugger from 'github-slugger'
 import { Content, Root } from 'mdast'
 import { fromMarkdown } from 'mdast-util-from-markdown'
-import { mdxFromMarkdown, MdxjsEsm } from 'mdast-util-mdx'
 import { toMarkdown } from 'mdast-util-to-markdown'
 import { toString } from 'mdast-util-to-string'
-import { mdxjs } from 'micromark-extension-mdxjs'
 import 'openai'
 import { Configuration, OpenAIApi } from 'openai'
 import { basename, dirname, join } from 'path'
 import { u } from 'unist-builder'
-import { filter } from 'unist-util-filter'
 import { inspect } from 'util'
 import yargs from 'yargs'
 
 dotenv.config()
 
-const ignoredFiles = ['pages/404.mdx']
-
-/**
- * Extracts ES literals from an `estree` `ObjectExpression`
- * into a plain JavaScript object.
- */
-function getObjectFromExpression(node: ObjectExpression) {
-  return node.properties.reduce<
-    Record<string, string | number | bigint | true | RegExp | undefined>
-  >((object, property) => {
-    if (property.type !== 'Property') {
-      return object
-    }
-
-    const key = (property.key.type === 'Identifier' && property.key.name) || undefined
-    const value = (property.value.type === 'Literal' && property.value.value) || undefined
-
-    if (!key) {
-      return object
-    }
-
-    return {
-      ...object,
-      [key]: value,
-    }
-  }, {})
-}
-
-/**
- * Extracts the `meta` ESM export from the MDX file.
- *
- * This info is akin to frontmatter.
- */
-function extractMetaExport(mdxTree: Root) {
-  const metaExportNode = mdxTree.children.find((node): node is MdxjsEsm => {
-    return (
-      node.type === 'mdxjsEsm' &&
-      node.data?.estree?.body[0]?.type === 'ExportNamedDeclaration' &&
-      node.data.estree.body[0].declaration?.type === 'VariableDeclaration' &&
-      node.data.estree.body[0].declaration.declarations[0]?.id.type === 'Identifier' &&
-      node.data.estree.body[0].declaration.declarations[0].id.name === 'meta'
-    )
-  })
-
-  if (!metaExportNode) {
-    return undefined
-  }
-
-  const objectExpression =
-    (metaExportNode.data?.estree?.body[0]?.type === 'ExportNamedDeclaration' &&
-      metaExportNode.data.estree.body[0].declaration?.type === 'VariableDeclaration' &&
-      metaExportNode.data.estree.body[0].declaration.declarations[0]?.id.type === 'Identifier' &&
-      metaExportNode.data.estree.body[0].declaration.declarations[0].id.name === 'meta' &&
-      metaExportNode.data.estree.body[0].declaration.declarations[0].init?.type ===
-        'ObjectExpression' &&
-      metaExportNode.data.estree.body[0].declaration.declarations[0].init) ||
-    undefined
-
-  if (!objectExpression) {
-    return undefined
-  }
-
-  return getObjectFromExpression(objectExpression)
-}
+// This is not really needed atm, but let's keep it here for future reference
+// import { cleanMdFiles } from './clean-md-files'
+// cleanMdFiles().catch(console.error)
 
 /**
  * Splits a `mdast` tree into multiple trees based on
@@ -106,8 +41,6 @@ function splitTreeBy(tree: Root, predicate: (node: Content) => boolean) {
   }, [])
 }
 
-type Meta = ReturnType<typeof extractMetaExport>
-
 type Section = {
   content: string
   heading?: string
@@ -116,42 +49,21 @@ type Section = {
 
 type ProcessedMdx = {
   checksum: string
-  meta: Meta
   sections: Section[]
 }
 
 /**
- * Processes MDX content for search indexing.
- * It extracts metadata, strips it of all JSX,
- * and splits it into sub-sections based on criteria.
+ * Processes MD content for search indexing.
+ * It extracts metadata and splits it into sub-sections based on criteria.
  */
 function processMdxForSearch(content: string): ProcessedMdx {
   const checksum = createHash('sha256').update(content).digest('base64')
 
-  const mdxTree = fromMarkdown(content, {
-    extensions: [mdxjs()],
-    mdastExtensions: [mdxFromMarkdown()],
-  })
-
-  const meta = extractMetaExport(mdxTree)
-
-  // Remove all MDX elements from markdown
-  const mdTree = filter(
-    mdxTree,
-    (node) =>
-      ![
-        'mdxjsEsm',
-        'mdxJsxFlowElement',
-        'mdxJsxTextElement',
-        'mdxFlowExpression',
-        'mdxTextExpression',
-      ].includes(node.type)
-  )
+  const mdTree = fromMarkdown(content, {})
 
   if (!mdTree) {
     return {
       checksum,
-      meta,
       sections: [],
     }
   }
@@ -175,7 +87,6 @@ function processMdxForSearch(content: string): ProcessedMdx {
 
   return {
     checksum,
-    meta,
     sections,
   }
 }
@@ -194,7 +105,7 @@ async function walk(dir: string, parentPath?: string): Promise<WalkEntry[]> {
       const stats = await stat(path)
       if (stats.isDirectory()) {
         // Keep track of document hierarchy (if this dir has corresponding doc file)
-        const docPath = `${basename(path)}.mdx`
+        const docPath = `${basename(path)}.md`
 
         return walk(
           path,
@@ -223,14 +134,12 @@ async function walk(dir: string, parentPath?: string): Promise<WalkEntry[]> {
 
 abstract class BaseEmbeddingSource {
   checksum?: string
-  meta?: Meta
   sections?: Section[]
 
   constructor(public source: string, public path: string, public parentPath?: string) {}
 
   abstract load(): Promise<{
     checksum: string
-    meta?: Meta
     sections: Section[]
   }>
 }
@@ -239,8 +148,8 @@ class MarkdownEmbeddingSource extends BaseEmbeddingSource {
   type: 'markdown' = 'markdown'
 
   constructor(source: string, public filePath: string, public parentFilePath?: string) {
-    const path = filePath.replace(/^pages/, '').replace(/\.mdx?$/, '')
-    const parentPath = parentFilePath?.replace(/^pages/, '').replace(/\.mdx?$/, '')
+    const path = filePath.replace(/^pages/, '').replace(/\.md?$/, '')
+    const parentPath = parentFilePath?.replace(/^pages/, '').replace(/\.md?$/, '')
 
     super(source, path, parentPath)
   }
@@ -248,15 +157,13 @@ class MarkdownEmbeddingSource extends BaseEmbeddingSource {
   async load() {
     const contents = await readFile(this.filePath, 'utf8')
 
-    const { checksum, meta, sections } = processMdxForSearch(contents)
+    const { checksum, sections } = processMdxForSearch(contents)
 
     this.checksum = checksum
-    this.meta = meta
     this.sections = sections
 
     return {
       checksum,
-      meta,
       sections,
     }
   }
@@ -296,8 +203,7 @@ async function generateEmbeddings() {
 
   const embeddingSources: EmbeddingSource[] = [
     ...(await walk('pages'))
-      .filter(({ path }) => /\.mdx?$/.test(path))
-      .filter(({ path }) => !ignoredFiles.includes(path))
+      .filter(({ path }) => /\.md?$/.test(path))
       .map((entry) => new MarkdownEmbeddingSource('guide', entry.path)),
   ]
 
@@ -313,7 +219,7 @@ async function generateEmbeddings() {
     const { type, source, path, parentPath } = embeddingSource
 
     try {
-      const { checksum, meta, sections } = await embeddingSource.load()
+      const { checksum, sections } = await embeddingSource.load()
 
       // Check for existing page in DB and compare checksums
       const { error: fetchPageError, data: existingPage } = await supabaseClient
@@ -401,7 +307,6 @@ async function generateEmbeddings() {
             path,
             type,
             source,
-            meta,
             parent_page_id: parentPage?.id,
           },
           { onConflict: 'path' }
@@ -453,6 +358,9 @@ async function generateEmbeddings() {
           if (insertPageSectionError) {
             throw insertPageSectionError
           }
+
+          // Add delay after each request
+          await delay(500) // delay of 0.5 second
         } catch (err) {
           // TODO: decide how to better handle failed embeddings
           console.error(
@@ -484,6 +392,10 @@ async function generateEmbeddings() {
   }
 
   console.log('Embedding generation complete')
+}
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 async function main() {
